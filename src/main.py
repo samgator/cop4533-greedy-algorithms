74744c205ffa8b99acee8b8e3759b7aeeb9d6d37
samgator <sam.morsics@gmail.com>
2026-03-06 09:42:59 -0500

    Write skeleton for main.py and file read/write logic

--- src/main.py
+++ src/main.py
@@ -1,5 +1,38 @@
+import sys
+
+def fifo():
+    misses = 0
+    return misses
+
+def lru():
+    misses = 0
+    return misses
+
+def optff():
+    misses = 0
+    return misses
+
 def main():
-    print("")
+    # input file is argument 1 i.e. "../input/example1.in"
+    # output file is argument 2 i.e. "../output/example1.out"
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    # read file
+    with open(input_file, 'r') as f:
+        k, m = map(int, f.readline().split())
+        requests = list(map(int, f.readline().split()))
+
+    # calculate misses for each algorithm
+    fifo_misses = fifo(requests, k)
+    lru_misses = lru(requests, k)
+    optff_misses = optff(requests, k)
+
+    # write results to output file
+    with open(output_file, 'w') as f:
+        f.write(f"FIFO  : {fifo_misses}\n")
+        f.write(f"LRU   : {lru_misses}\n")
+        f.write(f"OPTFF : {optff_misses}\n")
 
 if __name__ == "__main__":
     main()
